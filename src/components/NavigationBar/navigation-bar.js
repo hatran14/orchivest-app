161cbabbb8feb44bdd56ab9b61bfa27b98e9a5bd
BoBeoBeo <baophamst@gmail.com>
2023-03-21 01:03:29 +0700

    Nav-bar da~ okie

--- src/components/NavigationBar/navigation-bar.js
+++ src/components/NavigationBar/navigation-bar.js
@@ -3,53 +3,32 @@ import SpeedIcon from '@mui/icons-material/Speed';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import WaterfallChartRoundedIcon from '@mui/icons-material/WaterfallChartRounded';
 import TuneRoundedIcon from '@mui/icons-material/TuneRounded';
-import { useState } from "react";
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 function NavBar() {
-    const {key, setKey} = useState('Dash')
+    const data = [
+      {icon : <SpeedIcon/>, label:'Dashboard'},
+      {icon : <CalendarMonthIcon/>, label:'Schedule'},
+      {icon : <WaterfallChartRoundedIcon />, label :'Statistics'},
+      {icon : <TuneRoundedIcon/>, label : 'Equipments'}
+    ]
     return (
         <div className={Style.navbar}>
             <div className={Style.title}>
                 Orchivest
             </div>
-            <List>
-              <ListItem className={Style.navDiv}>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <SpeedIcon className={Style.navIcon}/>
-                  </ListItemIcon>
-                  <ListItemText primary="Dashboard" />
+            <List className = {Style.list}>
+              {data.map((item)=>(
+                <ListItemButton key = {item.label} className = {Style.list_div}>
+                    <ListItemIcon className = {Style.icon}>{item.icon}</ListItemIcon>
+                    <ListItemText primary = {item.label}/>
                 </ListItemButton>
-              </ListItem>
-              <ListItem className={Style.navDiv}>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <CalendarMonthIcon className={Style.navIcon} />
-                  </ListItemIcon>
-                  <ListItemText primary="Schedule" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem className={Style.navDiv}>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <WaterfallChartRoundedIcon className={Style.navIcon}/>
-                  </ListItemIcon>
-                  <ListItemText primary="Statistics" />
-                </ListItemButton>
-              </ListItem>
-              <ListItem className={Style.navDiv}>
-                <ListItemButton>
-                  <ListItemIcon>
-                    <TuneRoundedIcon className={Style.navIcon} />
-                  </ListItemIcon>
-                  <ListItemText primary="Equipments" />
-                </ListItemButton>
-              </ListItem>
-        </List>
+              ))}
+            </List>
+            
         </div>
 
     );
